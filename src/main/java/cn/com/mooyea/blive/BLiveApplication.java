545dcae070296dd3e85138aea5c092f3591edd67
lideyang <lidy@skyvis.com.cn>
2023-06-02 09:19:51 +0800

    Initial commit

---
+++ src/main/java/cn/com/mooyea/blive/BLiveApplication.java
@@ -0,0 +1,13 @@
+package cn.com.mooyea.blive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BLiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BLiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cn/com/mooyea/blive/BLiveApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.com.mooyea.blive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BLiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
